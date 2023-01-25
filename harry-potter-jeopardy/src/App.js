873057bc05960e3d5a0089e9936a459270f1640e
lexigrant <lexidgrant@gmail.com>
2023-01-25 14:29:51 -0700

    made a play button when clicked shows the board and updated font

--- harry-potter-jeopardy/src/App.js
+++ harry-potter-jeopardy/src/App.js
@@ -1,141 +1,50 @@
 import { useState } from 'react';
-import { Card } from 'react-bootstrap';
 import './App.css';
+import triviaData from './data/triviaData';
 
 function App() {
 
 const [showQuestion, setShowQuestion] = useState(false)
+const [activeQuestion, setActiveQuestion] = useState(false)
+const [gameActive, setGameActive] = useState(false)
 
-  const dummy =
-    [
-      {
-        category: 1,
-        questions: [
-          {
-            value: 100
-          },
-          {
-            value: 200
-          },
-          {
-            value: 300
-          },
-          {
-            value: 400
-          },
-          {
-            value: 500
-          },
-        ],
-      },
-      {
-        category: 2,
-        questions: [
-          {
-            value: 100
-          },
-          {
-            value: 200
-          },
-          {
-            value: 300
-          },
-          {
-            value: 400
-          },
-          {
-            value: 500
-          },
-        ],
-      },
-      {
-        category: 3,
-        questions: [
-          {
-            value: 100
-          },
-          {
-            value: 200
-          },
-          {
-            value: 300
-          },
-          {
-            value: 400
-          },
-          {
-            value: 500
-          },
-        ],
-      },
-      {
-        category: 4,
-        questions: [
-          {
-            value: 100
-          },
-          {
-            value: 200
-          },
-          {
-            value: 300
-          },
-          {
-            value: 400
-          },
-          {
-            value: 500
-          },
-        ],
-      },
-      {
-        category: 5,
-        questions: [
-          {
-            value: 100
-          },
-          {
-            value: 200
-          },
-          {
-            value: 300
-          },
-          {
-            value: 400
-          },
-          {
-            value: 500
-          },
-        ],
-      },
-    ]
+
+
+    const flipCard = (question) => {
+      if(!showQuestion) {
+        setActiveQuestion(question)
+        setShowQuestion(true)
+      } 
+    }
+
 
   return (
     <div>
-      <h1 className="gameTitle">Harry Potter Jeopardy</h1>
+      <h1 style={{fontFamily: "HarryP-MVZ6w"}} className="gameTitle">Harry Potter Jeopardy</h1>
       <div className="bigContainer">
-        <div className="board">
-          {dummy.map(category => {
-            return (
-              <div className="categoryContainer">
-                <h2 className="categoryTitle">{"CATEGORY " + category.category}</h2>
-                <div className="categoryValuesContainer">
-                  {category.questions.map(question => {
-                    return(
-                    <div className="questionContainer" onClick={() => {
-                      setShowQuestion(true)
-                    }}>
-                       <p className="values">${question.value}</p>
-                    </div>)
-
-
-                  })}
-                </div>
+      {!gameActive ? <button style={{fontFamily: "HarryP-MVZ6w"}} onClick={() => {
+        setGameActive(true)
+      }}>Play</button> :
+      !showQuestion ? <div className="board">
+        {triviaData.map(category => {
+          return (
+            <div className="categoryContainer">
+              <h2 style={{fontFamily: "HarryP-MVZ6w"}} className="categoryTitle">{category.categoryName}</h2>
+              <div className="categoryValuesContainer">
+                {category.questions.map(question => {
+                  return(
+                  <div className="questionContainer" onClick={() => {
+                    flipCard(question.trivia)
+                    
+                  }}>
+                      <p style={{fontFamily: "HarryP-MVZ6w"}} className="values">${question.value}</p>
+                  </div>)
+                })}
               </div>
-            )
-
-          })}
-        </div>
+            </div>
+          )
+        })}
+      </div> : <p style={{fontFamily: "HarryP-MVZ6w"}}>{activeQuestion}</p> }
       </div>
     </div>
   )
